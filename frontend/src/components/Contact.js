6307724c3cad371db023dc3be98a06a9c3dfb5f7
Lata Chapadia <latarana4u@gmail.com>
2025-04-17 19:14:38 +0530

    changed API

--- frontend/src/components/Contact.js
+++ frontend/src/components/Contact.js
@@ -22,7 +22,7 @@ const Contact = () => {
 
     try {
       // Send data to the serverless function
-      const response = await axios.post('/api/contact', formData);
+      const response = await axios.post('/api/ContactAPI.js',formData)
       setStatus('Form submitted successfully!');
     } catch (error) {
       setStatus('Error submitting the form.');
